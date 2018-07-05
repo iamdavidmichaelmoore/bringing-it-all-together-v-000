a47000b38516b570374bdda7d529f7fb20a6414a
David Moore <hey@iamdavidmoore.com>
2018-07-05 02:24:16 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,13 +5,13 @@ class Dog
   attr_accessor :name, :breed
   attr_reader :id
 
-  def initialize(name: breed:)
+  def initialize(name:, breed:)
     @name = name
     @breed = breed
   end
 
   def self.create_table
-    sql = "CREATE TABLE dogs (
+    sql = "CREATE TABLE IF NOT EXISTS dogs (
       id INTEGER PRIMARY KEY,
       name TEXT,
       breed TEXT
